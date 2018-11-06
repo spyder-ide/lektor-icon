2420ddc2f4da2f3f782149442a5a8c500a3a39e3
C.A.M. Gerlach <widenetservices@gmail.com>
2018-11-05 18:26:29 -0600

    Refactor scripts to only load when needed and add configurable loader

---
+++ assets/static/js/main-blog.js
@@ -0,0 +1,46 @@
+;(function () {
+
+	'use strict';
+
+
+	var mainMenuSticky = function() {
+
+		var sticky = $('.js-sticky');
+
+		sticky.css('height', sticky.height());
+		$(window).resize(function(){
+			sticky.css('height', sticky.height());
+		});
+
+		var $section = $('.fh5co-main-nav');
+
+		$section.waypoint(function(direction) {
+
+		  	if (direction === 'down') {
+
+			    	$section.css({
+			    		'position' : 'fixed',
+			    		'top' : 0,
+			    		'width' : '100%',
+			    		'z-index' : 99999
+			    	}).addClass('fh5co-shadow');;
+			}
+
+		}, {
+	  		offset: '0px'
+		});
+
+	};
+
+
+
+
+	// Document on load
+	$(function(){
+
+		mainMenuSticky();
+
+	});
+
+
+}());
